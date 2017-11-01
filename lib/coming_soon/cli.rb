c0e49623fcdbe130e6d5557202c403f2da7e89ac
Adam Fudge <coolfudge@gmail.com>
2017-11-01 20:57:36 +0000

    getting basic intro for CLI and Movie classes up and running

--- lib/coming_soon/cli.rb
+++ lib/coming_soon/cli.rb
@@ -1,7 +1,34 @@
 class ComingSoon::CLI
 
   def call
-    puts "Movies coming soon:"
+    intro
+    opening_week
+    opening_month
+    menu
   end
 
+  def intro
+    puts "Welcome to Coming Soon, the gem!"
+  end
+
+  def opening_week
+    puts "Opening this week:"
+    ComingSoon::Movie.list_opening_week
+  end
+
+  def opening_month
+    puts "Opening this month:"
+    ComingSoon::Movie.list_opening_month
+  end
+
+  def menu
+
+  end
 end
+
+
+
+
+# would you like details of a particular move, which one =>
+# display list =>
+# option to keep asking until quit
--- lib/coming_soon/movie.rb
+++ lib/coming_soon/movie.rb
@@ -1,3 +1,11 @@
 class ComingSoon::Movie
+  def self.list_opening_week
+    puts "THOR RAGNAROK"
+    puts "A BAD MOM'S CHRISTMAS"
+  end
 
+  def self.list_opening_month
+    puts "DADDY'S HOME 2"
+    puts "MURDER EXPRESS"
+  end
 end
