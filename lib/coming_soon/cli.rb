c78c86783349ee86540a3661a3c450ff3076c0f0
Adam Fudge <coolfudge@gmail.com>
2017-11-01 23:07:36 +0000

    getting close to final version, basic interface is working

--- lib/coming_soon/cli.rb
+++ lib/coming_soon/cli.rb
@@ -8,6 +8,7 @@ class CLI
 
   def intro
     puts "Welcome to Coming Soon, the gem!"
+    # initialize movies!
   end
 
   def opening_month
@@ -18,17 +19,24 @@ class CLI
   def menu
     input = nil
     while (input != "exit")
-      puts "Enter the movie number to get a description or list to display the list of movies, or exit"
+      puts "Enter the movie number to get a description or enter list to display the list of movies, or exit"
       input = gets.strip
 
-      movie = Movie.find_by_position(input)
-      if movie != false
-        # movie.display_details
+      if input.to_i > 0
+        movie = Movie.find_by_position(input)
+        if movie != false
+          movie.display_details
+        else
+          puts "Please try different input"
+        end
+      elsif input == "list"
+        opening_month
+      elsif input == "exit"
+        puts "Thank you for using Coming Soon, the gem!"
+      else
+        puts "Please try different input"
       end
-
     end
-
-    puts "Thank you for using Coming Soon, the gem!"
   end
 end
 
