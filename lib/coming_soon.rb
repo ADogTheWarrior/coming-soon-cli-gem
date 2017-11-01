e362f802926559771d0ff20da0919a61795891da
Adam Fudge <coolfudge@gmail.com>
2017-11-01 00:29:39 +0000

    laying more ground work for the gem

--- lib/coming_soon.rb
+++ lib/coming_soon.rb
@@ -1,11 +1,3 @@
-
 require_relative "coming_soon/version"
 require_relative "coming_soon/cli"
-
-# module ComingSoon
-  # class Chatter
-  #   def say_hello
-  #     puts 'This is zerp. Coming in loud and clear. Over.'
-  #   end
-  # end
-# end
+require_relative "coming_soon/movie"
