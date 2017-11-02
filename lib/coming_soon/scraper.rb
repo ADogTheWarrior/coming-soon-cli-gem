99c315cab37e2100cf102cfffe9b51187a214592
Adam Fudge <coolfudge@gmail.com>
2017-11-02 21:42:30 +0000

    created more fake data to use to test other classes

--- lib/coming_soon/scraper.rb
+++ lib/coming_soon/scraper.rb
@@ -1,10 +1,22 @@
 class Scraper
   def self.this_week
-    Movie.new("Thor", "nov 3")
+    Movie.new(name: "Thor",
+    date: "nov 3",
+    mp_rating: "PG-13",
+    time: "140 mins",
+    description: "Lorem Ipsum ...",
+    themes: ["Comedy", "Action"],
+    actors: ["Matt Damon", "Chris Hemsworth"])
   end
 
   def self.this_month
-    Movie.new("It", "nov 24")
+    Movie.new(name: "It",
+    date: "nov 24",
+    mp_rating: "R",
+    time: "130 mins",
+    description: "Lorem Ipsum",
+    themes: ["Horror", "Action"],
+    actors: ["Matt Damon", "Brad Pitt"])
   end
 
   def self.get_details(url)
