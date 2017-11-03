929160aacc5697f7d31736fd2b5e00a6c3a403d2
Adam Fudge <coolfudge@gmail.com>
2017-11-03 18:31:55 +0000

    getting more information scraped off main page

--- lib/coming_soon/scraper.rb
+++ lib/coming_soon/scraper.rb
@@ -10,31 +10,33 @@ class Scraper
       new_movie_hash[:date] = date
       new_movie_hash[:name] = movie.css("h4").css("a").first.attribute("title").text
       new_movie_hash[:url] = "http://www.imdb.com" + movie.css("h4").css("a").first.attribute("href").text
+      new_movie_hash[:mp_rating] = movie.css("p").first.children[1].attribute("title").value
+      new_movie_hash[:description] = movie.css(".outline").text.strip
+      # new_movie_hash[:time]
+      # new_movie_hash[:themes]
+      # new_movie_hash[:actors]
       return_array_of_hash << new_movie_hash
 # binding.pry
     end
-            # binding.pry
+# binding.pry
     return_array_of_hash
   end
 
   def self.this_month
     doc = Nokogiri::HTML(open("http://www.imdb.com/movies-coming-soon/"))
-
-    # Movie.new(name: "It",
-    # date: "nov 24",
-    # mp_rating: "R",
-    # time: "130 mins",
-    # description: "Lorem Ipsum",
-    # themes: ["Horror", "Action"],
-    # actors: ["Matt Damon", "Brad Pitt"])
-    #
-    # Movie.new(name: "Thor",
-    # date: "nov 3",
-    # mp_rating: "PG-13",
-    # time: "140 mins",
-    # description: "Lorem Ipsum ...",
-    # themes: ["Comedy", "Action"],
-    # actors: ["Matt Damon", "Chris Hemsworth"])
+    return_array_of_hash = []
+    # date = doc.css(".sub-list")
+    movies = doc.css(".overview-top")
+    movies.each do |movie|
+      new_movie_hash = {}
+      # new_movie_hash[:date] = date
+      new_movie_hash[:name] = movie.css("h4").css("a").first.attribute("title").text
+      new_movie_hash[:url] = "http://www.imdb.com" + movie.css("h4").css("a").first.attribute("href").text
+      return_array_of_hash << new_movie_hash
+binding.pry
+    end
+# binding.pry
+    return_array_of_hash
   end
 
   def self.get_details(movie_url)
