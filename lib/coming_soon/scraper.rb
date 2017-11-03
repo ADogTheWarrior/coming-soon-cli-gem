51e5da2721f8cc66cfcb2d85cce01777f94e8608
Adam Fudge <coolfudge@gmail.com>
2017-11-03 20:51:00 +0000

    starting work on get_details method, touched up this_month method

--- lib/coming_soon/scraper.rb
+++ lib/coming_soon/scraper.rb
@@ -1,26 +1,26 @@
 class Scraper
-  def self.this_week
-    doc = Nokogiri::HTML(open("http://www.imdb.com/movies-in-theaters/"))
-    return_array_of_hash = []
-    date = doc.css(".sub-list").css(".list").css("h3").children.first.text.split("- ")[1]
-    #even after .strip date has one trailing whitespace but its ascii like \u{a0}
-    movies = doc.css(".overview-top")
-    movies.each do |movie|
-      new_movie_hash = {}
-      new_movie_hash[:date] = date
-      new_movie_hash[:name] = movie.css("h4").css("a").first.attribute("title").text
-      new_movie_hash[:url] = "http://www.imdb.com" + movie.css("h4").css("a").first.attribute("href").text
-      if movie.css("p").children[1].attribute("title") != nil
-        new_movie_hash[:mp_rating] = movie.css("p").children[1].attribute("title").value
-      else
-        new_movie_hash[:mp_rating] = "Not Rated"
-      end
-      new_movie_hash[:description] = movie.css(".outline").text.strip
-      new_movie_hash[:time] = movie.css("p").children[3].children.text
-      return_array_of_hash << new_movie_hash
-    end
-    return_array_of_hash
-  end
+  # def self.this_week
+  #   doc = Nokogiri::HTML(open("http://www.imdb.com/movies-in-theaters/"))
+  #   return_array_of_hash = []
+  #   # date = doc.css(".sub-list").css(".list").css("h3").children.first.text.split("- ")[1]
+  #   #even after .strip date has one trailing whitespace but its ascii like \u{a0}
+  #   movies = doc.css(".overview-top")
+  #   movies.each do |movie|
+  #     new_movie_hash = {}
+  #     # new_movie_hash[:date] = date
+  #     new_movie_hash[:name] = movie.css("h4").css("a").first.attribute("title").text
+  #     new_movie_hash[:url] = "http://www.imdb.com" + movie.css("h4").css("a").first.attribute("href").text
+  #     if movie.css("p").children[1].attribute("title") != nil
+  #       new_movie_hash[:mp_rating] = movie.css("p").children[1].attribute("title").value
+  #     else
+  #       new_movie_hash[:mp_rating] = "Not Rated"
+  #     end
+  #     new_movie_hash[:description] = movie.css(".outline").text.strip
+  #     new_movie_hash[:time] = movie.css("p").children[3].children.text
+  #     return_array_of_hash << new_movie_hash
+  #   end
+  #   return_array_of_hash
+  # end
 
   def self.this_month
     doc = Nokogiri::HTML(open("http://www.imdb.com/movies-coming-soon/"))
@@ -37,15 +37,23 @@ class Scraper
         new_movie_hash[:mp_rating] = "Not Rated"
       end
       new_movie_hash[:description] = movie.css(".outline").text.strip
-      new_movie_hash[:time] = movie.css("p").children[3].children.text
       return_array_of_hash << new_movie_hash
     end
     return_array_of_hash
   end
 
   def self.get_details(movie_url)
+    puts "Scraping..."
+    doc = Nokogiri::HTML(open(movie_url))
+    attributes_hash = {}
     # needs date, genres, actors
     # if no rating then the time is off
-    doc = Nokogiri::HTML(open(movie_url))
+
+    attributes_hash[:date] = "Nov 5"
+    # attributes_hash[:themes]
+    # attributes_hash[:actors]
+    # attributes_hash[:time]
+
+    attributes_hash
   end
 end
