3536e276813f445899ff64bd613f569fe7ecb40e
Adam Fudge <coolfudge@gmail.com>
2017-11-03 19:06:45 +0000

    still working on bugs involving scrapping missinformation

--- lib/coming_soon/scraper.rb
+++ lib/coming_soon/scraper.rb
@@ -10,36 +10,42 @@ class Scraper
       new_movie_hash[:date] = date
       new_movie_hash[:name] = movie.css("h4").css("a").first.attribute("title").text
       new_movie_hash[:url] = "http://www.imdb.com" + movie.css("h4").css("a").first.attribute("href").text
-      new_movie_hash[:mp_rating] = movie.css("p").first.children[1].attribute("title").value
+      if movie.css("p").children[1].attribute("title") != nil
+        new_movie_hash[:mp_rating] = movie.css("p").children[1].attribute("title").value
+      else
+        new_movie_hash[:mp_rating] = "Not Rated"
+      end
       new_movie_hash[:description] = movie.css(".outline").text.strip
-      # new_movie_hash[:time]
-      # new_movie_hash[:themes]
-      # new_movie_hash[:actors]
+      new_movie_hash[:time] = movie.css("p").children[3].children.text
       return_array_of_hash << new_movie_hash
-# binding.pry
     end
-# binding.pry
     return_array_of_hash
   end
 
   def self.this_month
     doc = Nokogiri::HTML(open("http://www.imdb.com/movies-coming-soon/"))
     return_array_of_hash = []
-    # date = doc.css(".sub-list")
     movies = doc.css(".overview-top")
     movies.each do |movie|
+# binding.pry
       new_movie_hash = {}
-      # new_movie_hash[:date] = date
       new_movie_hash[:name] = movie.css("h4").css("a").first.attribute("title").text
       new_movie_hash[:url] = "http://www.imdb.com" + movie.css("h4").css("a").first.attribute("href").text
+      if movie.css("p").children[1].attribute("title") != nil
+        new_movie_hash[:mp_rating] = movie.css("p").children[1].attribute("title").value
+      else
+        new_movie_hash[:mp_rating] = "Not Rated"
+      end
+      new_movie_hash[:description] = movie.css(".outline").text.strip
+      new_movie_hash[:time] = movie.css("p").children[3].children.text
       return_array_of_hash << new_movie_hash
-binding.pry
     end
-# binding.pry
     return_array_of_hash
   end
 
   def self.get_details(movie_url)
+    # needs date, genres, actors
+    # if no rating then the time is off
     doc = Nokogiri::HTML(open(movie_url))
   end
 end
