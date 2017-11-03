419342cf32aa43d9f2f6f82e06e495e16a96c886
Adam Fudge <coolfudge@gmail.com>
2017-11-03 00:59:41 +0000

    working on getting url for each movie

--- lib/coming_soon/scraper.rb
+++ lib/coming_soon/scraper.rb
@@ -2,40 +2,21 @@ class Scraper
   def self.this_week
     doc = Nokogiri::HTML(open("http://www.imdb.com/movies-in-theaters/"))
     return_array_of_hash = []
-
-    #even after .strip has one trailing whitespace
-    date = doc.css(".sub-list").css(".list").css("h3").children.first.text.split("-")[1].strip
-    # title = doc.css(".overview-top").first.css("h4").css("a")
-    # url = doc.css(".overview-top").first.css("h4").css("a")
-    binding.pry
-
-    # movies.each do |movie|
-    #   new_movie_hash = {}
-    #   new_movie_hash[:name] = movie.css("h4").text
-    #   new_movie_hash[:date] = movie.css("p").text
-    #   new_movie_hash[:url] = movie.css("a").attribute("href").value
-    #
-    #   return_array_of_hash << new_movie_hash
-    # end
-
+    #even after .strip has one trailing whitespace but its ascii like \u{a0}
+    date = doc.css(".sub-list").css(".list").css("h3").children.first.text.split("- ")[1]
+    movies = doc.css(".overview-top")
+    movies.each do |movie|
+      new_movie_hash = {}
+      # title = movie.css("h4").css("a")
+      # url = movie.css("h4").css("a").first.attributes.first[-1]
+      return_array_of_hash << new_movie_hash
+        binding.pry
+    end
     return_array_of_hash
   end
 
   def self.this_month
     doc = Nokogiri::HTML(open("http://www.imdb.com/movies-coming-soon/"))
-    # return_array_of_hash = []
-    # movies = doc.css(".student-card")
-    #
-    # movies.each do |movie|
-    #   new_movie_hash = {}
-    #   new_movie_hash[:name] = movie.css("h4").text
-    #   new_movie_hash[:date] = movie.css("p").text
-    #   new_movie_hash[:url] = movie.css("a").attribute("href").value
-    #
-    #   return_array_of_hash << new_movie_hash
-    # end
-    #
-    # return_array_of_hash
 
     Movie.new(name: "It",
     date: "nov 24",
